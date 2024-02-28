ea2d2a099bb6ba0222f2363a43cf5d0af1939cd8
nikhillodha1999 <118475684+nikhillodha1999@users.noreply.github.com>
2024-02-28 14:53:19 +0530

    My Portfolio

---
+++ javascript.js
@@ -0,0 +1,43 @@
+document.querySelector('.menu-toggle').addEventListener('click', function () {
+    document.querySelector('.navbar').classList.toggle('active');
+});
+
+
+
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer", "Software Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
+
+document.getElementById('submitBtn').addEventListener('click', function () {
+    // Retrieve form data
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var subject = document.getElementById('subject').value;
+    var message = document.getElementById('message').value;
+
+    // Create CSV content
+    var csvContent = "Name,Email,Subject,Message\n"; // Header row
+    csvContent += '"' + name + '","' + email + '","' + subject + '","' + message + '"\n';
+
+    // Send the form data to data.csv file
+    sendFormDataToCSV(csvContent);
+});
+
+function sendFormDataToCSV(csvContent) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'data.csv', true); // Assuming data.csv is in the same directory
+    xhr.setRequestHeader('Content-type', 'text/csv; charset=utf-8');
+    xhr.send(csvContent);
+
+    // Clear form fields
+    document.getElementById('name').value = '';
+    document.getElementById('email').value = '';
+    document.getElementById('subject').value = '';
+    document.getElementById('message').value = '';
+}
+
